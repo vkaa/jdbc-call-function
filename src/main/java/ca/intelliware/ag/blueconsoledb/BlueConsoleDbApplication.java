0af427c4555e8cde09010243e8e00fd7ee83bbfe
Victor Savkov <victor.savkov@intelliware.ca>
2018-01-31 16:06:33 -0500

    initial commit: jdbc properties are set to connect to BlueConsole database

---
+++ src/main/java/ca/intelliware/ag/blueconsoledb/BlueConsoleDbApplication.java
@@ -0,0 +1,12 @@
+package ca.intelliware.ag.blueconsoledb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueConsoleDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueConsoleDbApplication.class, args);
+	}
+}
---
+++ src/test/java/ca/intelliware/ag/blueconsoledb/BlueConsoleDbApplicationTests.java
@@ -0,0 +1,16 @@
+package ca.intelliware.ag.blueconsoledb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlueConsoleDbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
