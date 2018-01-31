8647a8022775ea9a9755a934f7d5e623ef9e1c6b
Victor Savkov <victor.savkov@intelliware.ca>
2018-01-31 16:56:00 -0500

    added logger

--- src/main/java/ca/intelliware/ag/blueconsoledb/controller/BlueResults.java
+++ src/main/java/ca/intelliware/ag/blueconsoledb/controller/BlueResults.java
@@ -1,5 +1,7 @@
 package ca.intelliware.ag.blueconsoledb.controller;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/blue-results")
 public class BlueResults {
 
+    private static Logger logger = LogManager.getLogger(BlueResults.class);
+
     @GetMapping("/calculate/{ranksetId}")
     public void calculate(@PathVariable Integer ranksetId) {
-        System.out.println(String.format("[BlueResults.calculate] ranksetId: %s", ranksetId+100));
+        logger.info(String.format("[calculate] ranksetId: %s", ranksetId));
     }
 }
